cacf4c113ed5714c4362dc747c173bd907c508f2
Daniel Oberski <daob@daob-desktop.(none)>
2009-04-29 12:51:22 +0200

    Modify input so matrices are written to file upon LISREL run + tests

--- parse_lisrel.py
+++ parse_lisrel.py
@@ -10,19 +10,15 @@ class LisrelInput:
 
     def __init__(self, path = '', text = ''):
         "Set self.input_text or fail."
-        if text == '':
+        if text == '': # read from file
             input_file = open(path, 'rb')
             self.input_text = input_file.read()
             input_file.close()
+            self.path = path # remember path for write
         else:
             self.input_text = text
-
-    def get_matrix_forms(self):
-        """Retrieve 'form' (full, symmetrix, zero, diagonal, ... 
-           and free or fixed) Of matrices for LISREL model for each group.
-           Returns an <ngroups> list of dictionaries."""
-    
-        mats = { 'LY': {'Form':'FU', 'Free':'FI'},
+        self.re_flags = re.MULTILINE | re.IGNORECASE 
+        self.mats = { 'LY': {'Form':'FU', 'Free':'FI'},
              'LX': {'Form':'FU', 'Free':'FI'}, 
              'TE': {'Form':'DI', 'Free':'FR'}, 
              'TD': {'Form':'DI', 'Free':'FR'}, 
@@ -34,7 +30,14 @@ class LisrelInput:
              'TX': {'Form':'DI', 'Free':'FI'}, 
              'KA': {'Form':'DI', 'Free':'FI'}, 
              'AL': {'Form':'DI', 'Free':'FI'} }
-    
+
+
+    def get_matrix_forms(self):
+        """Retrieve 'form' (full, symmetrix, zero, diagonal, ... 
+           and free or fixed) Of matrices for LISREL model for each group.
+           Returns an <ngroups> list of dictionaries."""
+            
+        mats = self.mats
         ngroups = self.get_ngroups()
         forms = []
         start_re = r'^[ \t]*MO[A-Z0-9=, \t]+'
@@ -44,8 +47,7 @@ class LisrelInput:
         
             for key in mats.keys():
                 find_n = start_re + key + r'[ ]*=[ ]*([A-Z,]+)'
-                find = re.findall(find_n, self.input_text, re.MULTILINE | 
-                        re.IGNORECASE)
+                find = re.findall(find_n, self.input_text, self.re_flags)
                 if len(find) > 0:
                     tmp = find[igroup].split(',')
                     if len(tmp) > 1:
@@ -60,8 +62,7 @@ class LisrelInput:
     def get_ngroups(self):
         "Retrieve number of groups. Fails if file contains stacked analyses."
         model_line = r'^[ \t]*MO[^N]+'
-        res = re.findall(model_line, self.input_text, re.MULTILINE | 
-                re.IGNORECASE)
+        res = re.findall(model_line, self.input_text, self.re_flags)
         return(len(res))
     
     def get_dimensions(self):
@@ -77,8 +78,26 @@ class LisrelInput:
         
             for key in nvar.keys():
                 find_n = start_re + key + r'[ ]*=[ ]*([0-9]+)'
-                find = re.findall(find_n, self.input_text, re.MULTILINE | 
-                        re.IGNORECASE)
+                find = re.findall(find_n, self.input_text, self.re_flags)
                 if len(find) > 0:
                     dims[igroup][key] = int(find[igroup])
         return(dims)
+
+    def get_modified_input(self):
+        """Modifies input to write matrix results to files. Returns string."""
+        outstr = ' '.join(["%s=%s.out"%(key, key) for key in self.mats.keys()])
+        reg_out = re.compile(r'^[ ]*OU([A-Z0-9=.\'"]+)', self.re_flags)
+        return(reg_out.sub(r'OU ' + outstr, self.input_text))
+
+    def write_to_file(self, new_text, path = ''):
+        """Write the input text to a file. Overwrites the original 
+           (making a backup) if no argument is given."""
+        if path == '':
+            path = self.path
+            b_file = open(path + '.backup', 'w')
+            b_file.write(self.input_text)
+            b_file.close()
+
+        w_file = open(path, 'w')
+        w_file.write(new_text)
+        w_file.close()
--- tests/test_parse_lisrel.py
+++ tests/test_parse_lisrel.py
@@ -8,11 +8,55 @@ sys.path.append(base_path)
 from parse_lisrel import LisrelInput
 
 
-class TestParseFunctions(unittest.TestCase):
+class TestInputGB(unittest.TestCase):
 
     def setUp(self):
         self.test_input = LisrelInput(os.path.join(base_path, 
-                            'tests/ess-round2/r2jobPT.LS8'))
+                            'tests/ess-round3/GB/IMSMETN/r3imsmetnGB.LS8'))
+
+    def test_get_ngroups(self):
+        self.assertEqual(self.test_input.get_ngroups(), 3)
+
+    def test_get_dimensions(self):
+        dim = self.test_input.get_dimensions()
+        self.assertEqual(dim[0]['NX'], 0)
+        self.assertEqual(dim[0]['NY'], 12)
+        self.assertEqual(dim[0]['NK'], 7)
+        self.assertEqual(dim[0]['NE'], 12)
+        self.assertEqual(dim[1]['NX'], 0)
+        self.assertEqual(dim[1]['NY'], 12)
+        self.assertEqual(dim[1]['NK'], 7)
+        self.assertEqual(dim[1]['NE'], 12)
+
+    def test_get_matrix_forms(self):
+        forms = self.test_input.get_matrix_forms()
+        self.assertEqual(len(forms), 3)
+        self.assertEqual(forms[0]['LY'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[0]['TE'], {'Form':'SY', 'Free':'FI'})
+        self.assertEqual(forms[0]['PS'], {'Form':'SY', 'Free':'FI'})
+        self.assertEqual(forms[0]['BE'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[0]['GA'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[0]['PH'], {'Form':'SY', 'Free':'FI'})
+        self.assertEqual(forms[1]['LY'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[1]['TE'], {'Form':'SY', 'Free':'FI'})
+        self.assertEqual(forms[1]['PS'], {'Form':'SY', 'Free':'IN'})
+        self.assertEqual(forms[1]['BE'], {'Form':'FU', 'Free':'IN'})
+        self.assertEqual(forms[1]['GA'], {'Form':'FU', 'Free':'IN'})
+        self.assertEqual(forms[1]['PH'], {'Form':'SY', 'Free':'IN'})
+        self.assertEqual(forms[2]['LY'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[2]['TE'], {'Form':'SY', 'Free':'FI'})
+        self.assertEqual(forms[2]['PS'], {'Form':'SY', 'Free':'IN'})
+        self.assertEqual(forms[2]['BE'], {'Form':'FU', 'Free':'IN'})
+        self.assertEqual(forms[2]['GA'], {'Form':'FU', 'Free':'IN'})
+        self.assertEqual(forms[2]['PH'], {'Form':'SY', 'Free':'IN'})
+
+
+class TestInputPT(unittest.TestCase):
+
+    def setUp(self):
+        self.path = os.path.join(base_path, 
+                            'tests/ess-round2/r2jobPT.LS8')
+        self.test_input = LisrelInput(self.path)
 
     def test_get_ngroups(self):
         self.assertEqual(self.test_input.get_ngroups(), 2)
@@ -44,8 +88,29 @@ class TestParseFunctions(unittest.TestCase):
         self.assertEqual(forms[1]['GA'], {'Form':'FU', 'Free':'IN'})
         self.assertEqual(forms[1]['PH'], {'Form':'SY', 'Free':'IN'})
 
+    def input_writes_output(self, input_obj):
+        import re
+        str = input_obj.get_modified_input()
+        reg = re.compile(r'OU[^\n\r$]+PH[ ]*=[ ]*[A-Z]+\.[A-Z]', 
+                input_obj.re_flags)
+        return(len(reg.findall(str)) > 0)
+    
+    def test_get_modified_input(self):
+        self.assert_(self.input_writes_output(self.test_input))
+
+    def test_write_to_file(self):
+        str = self.test_input.get_modified_input()
+        self.test_input.write_to_file(str)
+        orig_str = self.test_input.input_text
+        f = open(self.path + '.backup')
+        self.assertEqual(f.read(), orig_str)
+        f.close()
+        new_input = LisrelInput(self.path)
+        self.assert_(self.input_writes_output(new_input))
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestParseFunctions)
+    suite_PT = unittest.TestLoader().loadTestsFromTestCase(TestInputPT)
+    suite_GB = unittest.TestLoader().loadTestsFromTestCase(TestInputGB)
+    suite = unittest.TestSuite([suite_PT, suite_GB])
     unittest.TextTestRunner(verbosity=2).run(suite)
 
