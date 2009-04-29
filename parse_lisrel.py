b9f8e1d1f5b7967c72f7128c427247af8a60684c
Daniel Oberski <daob@daob-desktop.(none)>
2009-04-29 09:46:40 +0200

    added get_matrix_forms

--- parse_lisrel.py
+++ parse_lisrel.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 import re
+from copy import deepcopy
 
 
-mats = { 'LY': {'Form':'FU', 'Free':'FI'},
+def get_matrix_forms(input):
+    """Retrieve 'form' (full, symmetrix, zero, diagonal, ... and free or fixed) 
+       Of matrices for LISREL model for each group.
+       Returns an <ngroups> list of dictionaries."""
+
+    mats = { 'LY': {'Form':'FU', 'Free':'FI'},
          'LX': {'Form':'FU', 'Free':'FI'}, 
          'TE': {'Form':'DI', 'Free':'FR'}, 
          'TD': {'Form':'DI', 'Free':'FR'}, 
@@ -13,17 +19,38 @@ mats = { 'LY': {'Form':'FU', 'Free':'FI'},
          'TY': {'Form':'DI', 'Free':'FI'}, 
          'TX': {'Form':'DI', 'Free':'FI'}, 
          'KA': {'Form':'DI', 'Free':'FI'}, 
-         'AL': {'Form':'DI', 'Free':'FI'} 
-        }
+         'AL': {'Form':'DI', 'Free':'FI'} }
+
+    ng = get_ngroups(input)
+    forms = []
+    start_re = r'^[ \t]*MO[A-Z0-9=, \t]+'
+
+    for ig in range(ng):
+        forms.append(deepcopy(mats))
+    
+        for n in mats.keys():
+            find_n = start_re + n + r'[ ]*=[ ]*([A-Z,]+)'
+            find = re.findall(find_n, input, re.MULTILINE | re.IGNORECASE)
+            if len(find) > 0:
+                tmp = find[ig].split(',')
+                if len(tmp) > 1:
+                    forms[ig][n]['Form'] = tmp[0].upper()
+                    forms[ig][n]['Free'] = tmp[1].upper()
+                elif tmp[0].upper() == 'IN':
+                    forms[ig][n]['Form'] = forms[ig - 1][n]['Form']
+                    forms[ig][n]['Free'] = 'IN'
+    return(forms)
 
 def get_ngroups(input):
+    "Retrieve number of groups. Fails if file contains stacked analyses."
     model_line = r'^[ \t]*MO[^N]+'
     res = re.findall(model_line, input, re.MULTILINE | re.IGNORECASE)
     return(len(res))
 
 
 def get_dimensions(input):
-    from copy import deepcopy
+    """Retrieve number of x, y, eta, and ksi variables for each group.
+       Returns a <ngroups> list of dictionaries."""
 
     ng = get_ngroups(input)
     dims = []
--- tests/test_parse_lisrel.py
+++ tests/test_parse_lisrel.py
@@ -30,6 +30,22 @@ class TestParseFunctions(unittest.TestCase):
         self.assertEqual(dim[1]['NK'], 6)
         self.assertEqual(dim[1]['NE'], 9)
 
+    def test_get_matrix_forms(self):
+        forms = parse_lisrel.get_matrix_forms(self.test_input)
+        self.assertEqual(len(forms), 2)
+        self.assertEqual(forms[0]['LY'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[0]['TE'], {'Form':'SY', 'Free':'FI'})
+        self.assertEqual(forms[0]['PS'], {'Form':'DI', 'Free':'FI'})
+        self.assertEqual(forms[0]['BE'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[0]['GA'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[0]['PH'], {'Form':'SY', 'Free':'FI'})
+        self.assertEqual(forms[1]['LY'], {'Form':'FU', 'Free':'FI'})
+        self.assertEqual(forms[1]['TE'], {'Form':'SY', 'Free':'FI'})
+        self.assertEqual(forms[1]['PS'], {'Form':'DI', 'Free':'IN'})
+        self.assertEqual(forms[1]['BE'], {'Form':'FU', 'Free':'IN'})
+        self.assertEqual(forms[1]['GA'], {'Form':'FU', 'Free':'IN'})
+        self.assertEqual(forms[1]['PH'], {'Form':'SY', 'Free':'IN'})
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestParseFunctions)
